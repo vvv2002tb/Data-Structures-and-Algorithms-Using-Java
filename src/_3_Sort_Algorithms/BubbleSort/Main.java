be389d12d4fe7ed9ae88f8b3dbd181a53e13d6c8
vvv2002tb <MAC@MacBook-Pro.local>
2024-02-19 22:31:58 +0700

    upload

--- src/_3_Sort_Algorithms/BubbleSort/Main.java
+++ src/_3_Sort_Algorithms/BubbleSort/Main.java
@@ -2,6 +2,7 @@ package _3_Sort_Algorithms.BubbleSort;
 
 // Thuật toán sắp xếp nổi bọt
 // Hiệu suất của thuật toán này sẽ giảm nhanh chóng khi số mục (n) cần sắp xêp tăng lên.
+// Big (O) là:
 
 public class Main {
     public static void main(String[] args) {
